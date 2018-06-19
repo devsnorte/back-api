08bde1a8ff1896b9f7fc00765e48dfdc43edf9c8
Amimaro <amir.zahlan@gmail.com>
2018-06-19 10:46:12 -0300

    :sparkles: Get meetup members

--- model/meetup/controller.js
+++ model/meetup/controller.js
@@ -1,6 +1,16 @@
 const Controller = require('../../lib/controller')
 const meetupFacade = require('./facade')
+const axios = require('axios')
 
-class MeetupController extends Controller {}
+class MeetupController extends Controller {
+
+  members(req, res, next) {
+    axios.get(`https://api.meetup.com/Vue-js-Norte/members?key=${process.env.MEETUP_APIKEY}`)
+    .then(response => {
+      res.json(response.data)
+    })
+  }
+
+}
 
 module.exports = new MeetupController(meetupFacade)
