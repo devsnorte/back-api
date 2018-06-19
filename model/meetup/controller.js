11611d5eb3384aa8633477d4029caf3d4c9025ee
Amimaro <amir.zahlan@gmail.com>
2018-06-19 11:54:13 -0300

    :sparkles: Get meetup info and events

--- model/meetup/controller.js
+++ model/meetup/controller.js
@@ -4,8 +4,22 @@ const axios = require('axios')
 
 class MeetupController extends Controller {
 
+  meetup(req, res, next) {
+    axios.get(`https://api.meetup.com/Vue-js-Norte?key=${process.env.MEETUP_APIKEY}`)
+    .then(response => {
+      res.json(response.data)
+    })
+  }
+
   members(req, res, next) {
     axios.get(`https://api.meetup.com/Vue-js-Norte/members?key=${process.env.MEETUP_APIKEY}`)
+    .then(response => {
+      res.json(response.data)
+    })
+  }
+
+  events(req, res, next) {
+    axios.get(`https://api.meetup.com/Vue-js-Norte/events?key=${process.env.MEETUP_APIKEY}`)
     .then(response => {
       res.json(response.data)
     })
--- model/meetup/router.js
+++ model/meetup/router.js
@@ -2,7 +2,11 @@ const controller = require('./controller')
 const Router = require('express').Router
 const router = new Router()
 
+router.route('/')
+.get((...args) => controller.meetup(...args))
 router.route('/members')
   .get((...args) => controller.members(...args))
+router.route('/events')
+  .get((...args) => controller.events(...args))
 
 module.exports = router
